d611be1336812a6cd8e5e974397f6942266600f2
HofmanRemco <remco.hofman@student.howest.be>
2018-02-24 11:26:13 +0100

    Added Band constructor

--- MyHobbies/Band.cs
+++ MyHobbies/Band.cs
@@ -6,6 +6,12 @@ namespace MyHobbies
 {
     class Band
     {
+        public Band(int jaar, String naam) {
+            Jaar = jaar;
+            Naam = naam;
+            Bandleden = new LinkedList<Bandlid>();
+        }
+
         public int Jaar { get; set; }
         public string Naam { get; set; }
         public LinkedList<Bandlid> Bandleden { get; set; }
